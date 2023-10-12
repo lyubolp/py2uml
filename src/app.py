1a094732b2acecf734eb7e5288189f5e8bc41dd7
Lyuboslav Karev <lyubo.karev@gmail.com>
2023-10-12 08:34:07 +0300

    Added return type to the main logic

--- src/app.py
+++ src/app.py
@@ -3,7 +3,8 @@ Main application logic
 """
 
 
-def generate_uml_class_diagram(source_files: list[str], output_file: str):
+def generate_uml_class_diagram(source_files: list[str], output_file: str) -> list[str]:
     """
     Generate UML class diagram from source folder
     """
+    return []
