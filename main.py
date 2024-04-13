274b50c25169720b3f99e867643f4fa80ffc746d
Lyuboslav Karev <lyubo.karev@gmail.com>
2024-04-13 22:15:11 +0300

    Added some more args

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 Main program
 """
 import argparse
+import os
+
 from src.app import generate_uml_class_diagram
+from src.file_utils import expand_directory
 
 
 def setup_cli():
@@ -10,18 +13,34 @@ def setup_cli():
     Setup CLI
     """
     parser = argparse.ArgumentParser(description='Generate UML class diagram from source folder')
-    parser.add_argument('source_files', metavar='source_files', type=str, nargs='+',
-                        help='Files to generate the diagram from')
+
+    parser.add_argument('source', metavar='source', type=str, nargs='+',
+                        help='Files/directories to generate the diagram from')
     parser.add_argument('output_dir', metavar='output_dir', type=str, help='Output directory')
 
+    parser.add_argument('-p', '--plantuml', action='store_true', default=True,
+                        help='Store PlantUML file (default)')
+    parser.add_argument('-i', '--image', action='store_true', default=True,
+                        help='Store image file (default)')
+    parser.add_argument('--version', action='version', version='Py2UML 1.0')
 
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = setup_cli()
-    input_files = args.source_files
-    output_file = args.output_dir
 
-    uml = generate_uml_class_diagram(input_files, output_file)
+    input_files = []
 
+    for source in args.source:
+        if os.path.isdir(source):
+            input_files += expand_directory(source)
+        elif os.path.isfile(source):
+            input_files.append(source)
+        else:
+            # TODO - Logging
+            print(f"Cannot find file or directory: {source}")
+
+    output_dir = args.output_dir
+
+    generate_uml_class_diagram(input_files, output_dir, args.plantuml, args.image)
