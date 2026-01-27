3f16ed43d477c7ab3bc01528e44edad57b2738f8
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-01-27 11:10:04 +0200

    Update driver code

--- src/main.rs
+++ src/main.rs
@@ -11,48 +11,57 @@ use std::io::Write;
 use crate::class_diagram::model_to_uml;
 use crate::class_diagram::python_to_model::generate_models;
 
-use crate::cli::Args;
-use crate::dependency_graph::graph_to_uml::generate_plantuml;
+use crate::cli::{Args, DiagramType};
+use crate::dependency_graph::graph_to_uml;
 use crate::dependency_graph::python_to_graph::build_dependency_graph;
 use crate::utils::file_utils::discover_files;
 
-fn run() -> Result<(), String> {
+fn main() {
     let args = Args::parse();
-    args.validate()?;
+    let args_validation = args.validate();
+
+    if let Err(e) = args_validation {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
 
     let files = discover_files(args.input_path.to_str().unwrap());
-    let graph = build_dependency_graph(files, args.input_path.to_str().unwrap());
-    let content = generate_plantuml(&graph);
 
-    let mut file = File::create(&args.output_path)
-        .map_err(|e| format!("Failed to create output file: {}", e))?;
+    let content = match args.diagram_type {
+        DiagramType::Class => {
+            println!("Generating class diagram...");
+            let models = generate_models(&vec![String::from("/Users/lyubolp/py2uml/sample.py")]);
+            model_to_uml::generate_plantuml(&models)
+        }
+        DiagramType::Dependency => {
+            println!("Generating dependency diagram...");
+            let graph = build_dependency_graph(files, args.input_path.to_str().unwrap());
+            graph_to_uml::generate_plantuml(&graph)
+        }
+    };
 
-    for line in content {
-        writeln!(file, "{}", line).map_err(|e| format!("Failed to write to output file: {}", e))?;
+    if let Ok(mut file) = File::create(&args.output_path) {
+        for line in content {
+            let write_result = writeln!(file, "{}", line);
+            if let Err(e) = write_result {
+                eprintln!(
+                    "Error: Failed to write to output file '{}': {}",
+                    args.output_path.display(),
+                    e
+                );
+                std::process::exit(1);
+            }
+        }
+    } else {
+        eprintln!(
+            "Error: Failed to create output file '{}'",
+            args.output_path.display()
+        );
+        std::process::exit(1);
     }
 
-    Ok(())
-}
-
-fn main() {
-    // if let Err(e) = run() {
-    //     eprintln!("Error: {}", e);
-    //     std::process::exit(1);
-    // }
-
-    let models = generate_models(&vec![String::from("/Users/lyubolp/py2uml/sample.py")]);
-
-    let content = model_to_uml::generate_plantuml(&models);
-
-    println!("{:#?}", models);
-
-    let mut file = File::create("sample.puml")
-        .map_err(|e| format!("Failed to create output file: {}", e))
-        .unwrap();
-
-    for line in content {
-        writeln!(file, "{}", line)
-            .map_err(|e| format!("Failed to write to output file: {}", e))
-            .unwrap();
-    }
+    println!(
+        "PlantUML diagram successfully generated at '{}'",
+        args.output_path.display()
+    );
 }
