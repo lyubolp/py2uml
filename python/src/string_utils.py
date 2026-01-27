8ea121c8b5a2df7b5327ad3a35de820e4d779d8d
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-01-27 11:12:31 +0200

    Remove python code

--- python/src/string_utils.py
+++
@@ -1,8 +0,0 @@
-def get_indentation_level(line: str) -> int:
-    """
-    Get the indentation level of a line of code.
-
-    :param line: The line of code to check.
-    :return: The number of leading spaces in the line.
-    """
-    return len(line) - len(line.lstrip())
